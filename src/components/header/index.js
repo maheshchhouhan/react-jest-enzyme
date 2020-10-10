5ae6550e4a358310b07f196ee9d6b36250ec71d2
Mahesh Singh Chouhan <mahesh@futureshirts.com>
2020-10-11 05:20:06 +0530

    Header component and Tests

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,15 +3,15 @@ import './styles.scss';
 import Logo from './../../assets/graphics/logo.png';
 
 const Header = () => {
-    return (
-        <header data-test="headerComponent" className='headerComponent'>
-            <div className="wrap">
-                <div className="logo">
-                    <img data-test="logoIMG" src={Logo} alt="Logo" />
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header data-test='headerComponent'>
+      <div className='wrap'>
+        <div className='logo'>
+          <img data-test='logoIMG' src={Logo} alt='Logo' />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
